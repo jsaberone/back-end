851081e8f3313139078ac02fcb1dfc7174c3b2ae
zhouzijia <1600584370@qq.com>
2020-04-28 14:08:04 +0800

    分布式集群监听redis过期事件，不重复监听

--- spring-redis-cluster/rediscluster2/src/main/java/com/kk/rediscluster2/Rediscluster2Application.java
+++ spring-redis-cluster/rediscluster2/src/main/java/com/kk/rediscluster2/Rediscluster2Application.java
@@ -2,8 +2,10 @@ package com.kk.rediscluster2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Rediscluster2Application {
 
     public static void main(String[] args) {
--- spring-redis-cluster/rediscluster2/src/main/java/com/kk/rediscluster2/config/lettuce/KeyExpiredEventMessageListener.java
+++ spring-redis-cluster/rediscluster2/src/main/java/com/kk/rediscluster2/config/lettuce/KeyExpiredEventMessageListener.java
@@ -1,8 +1,10 @@
 package com.kk.rediscluster2.config.lettuce;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -17,12 +19,36 @@ import org.springframework.util.StringUtils;
 @Component
 public class KeyExpiredEventMessageListener implements MessageListener {
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Override
     public void onMessage(Message message, byte[] pattern) {
         String key = new String(message.getChannel());
         String action = new String(message.getBody());
-        if(!StringUtils.isEmpty(key) && key.contains("expired")) {
+        if(!StringUtils.isEmpty(key) && key.contains("expired") && action.startsWith("tracker") ) {
+            dohandleKey2(key, action);
+        }
+    }
+
+    /**
+     * @description 数据处理，分布式集群式处理，防止多次处理
+     * @param key, action
+     */
+    public void dohandleKey2(String key , String action){
+        String lockValue = redisTemplate.opsForValue().getAndSet("ex:"+ action, "1");
+        if("1".equals(lockValue)){
+            return;
+        }else{
             log.info("监听到的过期事件：{},redis key 是:{}", key, action);
+        }
+        //todo 处理自己的东西
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            redisTemplate.delete("ex:"+action);
         }
     }
 }
